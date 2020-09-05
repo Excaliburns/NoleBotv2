644d0b50a9765ea2714cc67b725455b074e02a07
apion <apion@apionservers.com>
2020-09-05 12:09:49 -0400

    Finished the help command, and fixed an issue with help command not showing the help page for the correct command

--- nolebotv2core/src/main/java/commands/general/Help.java
+++ nolebotv2core/src/main/java/commands/general/Help.java
@@ -2,6 +2,7 @@ package commands.general;
 
 import commands.util.Command;
 import commands.util.CommandEvent;
+import commands.util.CommandUtil;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import util.chat.EmbedHelper;
 
@@ -17,17 +18,32 @@ public class Help extends Command {
         requiredPermissionLevel = 0;
         usages.add("help");
         usages.add("help [command]");
+        usages.add("help [1-100]");
     }
 
     @Override
     public void onCommandReceived(CommandEvent event) throws Exception {
-        if (event.getMessageContent().size() == 2) {
+    	//You can replace the isNumber method with String.matches(regex) but I couldn't get the right regex
+    	//This logic will make it impossible to have a command only made of numbers, but I don't think that will be a problem
+        if (event.getMessageContent().size() == 1 || isNumber(event.getMessageContent().get(1))) {
+        	try {
+        		event.getChannel().sendMessage(sendHelpCommand(event)).queue();
+        	}
+        	catch (IndexOutOfBoundsException e)
+        	{
+        		//Not sure if its necessary to make this error message into a MessageEmbed
+        		event.getChannel().sendMessage("That help page doesn't exist!").queue();
+        	}
+        }
+        else if (event.getMessageContent().size() == 2) {
             event.getChannel().sendMessage(sendHelpSubcommand(event)).queue();
         }
     }
 
     private MessageEmbed sendHelpSubcommand(CommandEvent event) {
-        final Command calledCommand = event.getCommand();
+    	// Uses the map of commands in CommandUtil to match the second word of the message to a command
+        final Command calledCommand = CommandUtil.commands.get(CommandUtil.commandIndex.get(event.getMessageContent().get(1)));
+        		// event.getCommand() doesn't work here
         final String prefix         = event.getSettings().getPrefix();
         final String commandString  = prefix + calledCommand.getName();
 
@@ -41,5 +57,39 @@ public class Help extends Command {
         fieldList.add(new MessageEmbed.Field("Usages <required> [optional]:", usages.toString(),                                         false));
 
         return EmbedHelper.buildDefaultMessageEmbed(event.getOriginatingJDAEvent(), fieldList);
+    }
+    private MessageEmbed sendHelpCommand(CommandEvent event) {
+    	final String prefix             = event.getSettings().getPrefix();
+    	final int pageNum               = event.getMessageContent().size() == 1 ? 1 : Integer.valueOf(event.getMessageContent().get(1));
+    	ArrayList<Command> commands     = CommandUtil.commands;
+    	final int numPages              = commands.size() % 10 == 0 ? commands.size() / 10 : (commands.size() / 10) + 1;
+    	String info1                    = "";
+    	String info2                    = "";
+    	//Starting index of the loop
+    	int startIndex                  = (pageNum - 1) * 10;
+    	//End index of the loop. If there are less than 10 commands, loop through them all. Otherwise, only loop through 10 commands
+    	int endIndex                    = commands.size() < 10 ? commands.size() : startIndex + 10;
+    	List<MessageEmbed.Field> fields = new ArrayList<>();
+    	if (pageNum > numPages) {
+    		throw new IndexOutOfBoundsException();
+    	}
+    	// Starts at the startIndex command, then creates a field with the command name and the description.
+    	// Adds it to the list of fields, then loops for 10 commands (or less if there aren't 10 commands registered).
+    	for (int i = startIndex; i < endIndex; i++) {
+    		info1 = prefix + commands.get(i+((pageNum-1)*10)).getName() + ": ";
+    		info2 = commands.get(i).getDescription();
+    		MessageEmbed.Field temp = new MessageEmbed.Field(info1, info2, false);
+    		fields.add(temp);
+    	}
+    	return EmbedHelper.buildDefaultMessageEmbed(event.getOriginatingJDAEvent(), fields);
+    }
+    private boolean isNumber(String s) {
+    	String numbers = "0123456789";
+    	for (int i = 0; i < s.length(); i++) {
+    		if (!numbers.contains(""+s.charAt(i))){
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
