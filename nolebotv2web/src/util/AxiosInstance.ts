fd142d23d089f32fe1e2d782a1cae54bef96f565
Excaliburns <tutsandallthat@gmail.com>
2021-05-28 00:34:15 -0400

    Fix OAuth flow, backend -> frontend works

--- nolebotv2web/src/util/AxiosInstance.ts
+++ nolebotv2web/src/util/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://localhost:8080/',
+    baseURL: 'http://localhost:8080/',
     timeout: 1000
 })
 
--- nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/controllers/AuthController.java
+++ nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/controllers/AuthController.java
@@ -1,5 +1,9 @@
 package com.tut.nolebotv2webapi.controllers;
 
+import com.tut.nolebotv2webapi.client.DiscordApiClient;
+import com.tut.nolebotv2webapi.dtos.DiscordAccessTokenRequestDto;
+import com.tut.nolebotv2webapi.entities.DiscordAccessToken;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
@@ -11,13 +15,34 @@ import java.util.HashMap;
 
 @Controller("/oauth")
 public class AuthController {
+    @Property(name = "micronaut.security.oauth2.clients.discord.client-id")
+    protected String client_id;
+
+    @Property(name = "micronaut.security.oauth2.clients.discord.client-secret")
+    protected String client_secret;
+
+    @Property(name = "micronaut.application.base-ui-url")
+    protected String base_ui_url;
+
+    private final DiscordApiClient discordApiClient;
+
+    public AuthController(DiscordApiClient discordApiClient) {
+        this.discordApiClient = discordApiClient;
+    }
+
     @Post("/discord")
     @Produces(MediaType.APPLICATION_JSON)
-    public HttpResponse<HashMap<String, String>> discord(
-            @Body final String clientCode
+    public HttpResponse<DiscordAccessToken> discord(
+            final String clientCode
     ) {
+        final DiscordAccessToken token = discordApiClient.getAccessToken(
+                client_id,
+                client_secret,
+                "authorization_code",
+                clientCode,
+                base_ui_url
+        ).blockingSingle();
 
-
-        return HttpResponse.ok(hashMap);
+        return HttpResponse.ok(token);
     }
 }
