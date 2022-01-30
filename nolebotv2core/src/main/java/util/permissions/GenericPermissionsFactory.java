c7c32cb39090d7daace2d3ddc336942953ddcca5
Excaliburns <tutsandallthat@gmail.com>
2022-01-30 04:50:32 -0500

    Fix stuff

--- nolebotv2core/src/main/java/util/permissions/GenericPermissionsFactory.java
+++ nolebotv2core/src/main/java/util/permissions/GenericPermissionsFactory.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 public class GenericPermissionsFactory {
     private static final Logger logger = LogManager.getLogger(GenericPermissionsFactory.class);
+
     /**
      * Gets the set of permissions stored that are greater than or equal to permission
      * @param guild The guild object that the user to check is a member of
@@ -32,6 +33,7 @@ public class GenericPermissionsFactory {
                        .filter(each -> each.getPermissionLevel() >= permission.getPermissionLevel())
                        .collect(Collectors.toCollection(TreeSet::new));
     }
+
     /**
      * Gets the int permissionLevel for the user in the current guild, if there is one
      * @param userId The ID of the user to check permissions for
@@ -41,6 +43,7 @@ public class GenericPermissionsFactory {
     public static int getHighestPermissionForUser(String userId, Guild guild) {
         return getHighestPermissionObjectForUser(userId, guild).getPermissionLevel();
     }
+
     /**
      * Gets the GenericPermission object for specified user in the specified guild, if there is one
      * @param userId The ID of the user to check permissions for
@@ -50,6 +53,7 @@ public class GenericPermissionsFactory {
     public static GenericPermission getHighestPermissionObjectForUser(String userId, Guild guild) {
         return PermissionCache.getPermissionForUser(userId, guild.getId());
     }
+
     /**
      * Gets the set of permissions for the user in the current guild, if there is one
      * @param userId The ID of the user to check permissions for
@@ -62,38 +66,40 @@ public class GenericPermissionsFactory {
         //If the member exists gets tge
         if (member.isPresent()) {
             //The list of RoleIDs that the member holds
-            final List<String> memberRoleIds = member.get()
-                                                     .getRoles()
-                                                     .stream()
-                                                     .map(ISnowflake::getId)
-                                                     .collect(Collectors.toList());
+            final List<String> memberRoleIds = member.get().getRoles()
+                    .stream()
+                    .map(ISnowflake::getId)
+                    .collect(Collectors.toList());
             //The set of Snowflake IDs for the permissions stored in settings
             final Set<String> guildPermissionIds = settings.getPermissionList()
-                                                           .stream()
-                                                           .map(GenericPermission::getSnowflakeId)
-                                                           .collect(Collectors.toSet());
+                    .stream()
+                    .map(GenericPermission::getSnowflakeId)
+                    .collect(Collectors.toSet());
             //The set of the RoleIDs that the user holds that have a permission stored in settings
-            final Set<String> userPermissions = memberRoleIds.stream()
-                                                             .filter(guildPermissionIds::contains)
-                                                             .collect(Collectors.toCollection(TreeSet::new));
-            //The set of GenericPermissions that apply to the user from their roles + GenericPermissions defined for their UserID
+            final Set<String> userPermissions = memberRoleIds
+                    .stream()
+                    .filter(guildPermissionIds::contains)
+                    .collect(Collectors.toCollection(TreeSet::new));
+            // The set of GenericPermissions that apply to the user from their roles +
+            // GenericPermissions defined for their UserID
             final TreeSet<GenericPermission> userGenericPermissionList = settings.getPermissionList()
-                                                                                 .stream()
-                    //Gets a set of GenericPermissions that are stored in settings that correspond to the RoleIDs that match the snowflake ID of the permissions stored in settings
-                                                                                 .filter(ele -> userPermissions.stream()
-                                                                                                               .anyMatch(element -> ele
-                                                                                                               .getSnowflakeId()
-                                                                                                               .equals(element)))
-                                                                                 .collect(Collectors.toCollection(TreeSet::new));
+                    .stream()
+                    // Gets a set of GenericPermissions that are stored in settings that
+                    // correspond to the RoleIDs that match the snowflake ID of the permissions stored in settings
+                    .filter(ele -> userPermissions
+                            .stream()
+                            .anyMatch(element -> ele.getSnowflakeId().equals(element))
+                    )
+                    .collect(Collectors.toCollection(TreeSet::new));
 
             // add user permission to list if present
             settings.getPermissionList()
                     .stream()
-                    //Gets the Set of GenericPermissions that are stored for a user
+                    // Gets the Set of GenericPermissions that are stored for a user
                     .filter(entry -> userId.equals(entry.getSnowflakeId()))
-                    //Checks if there are any GenericPermissions stored for a user
+                    // Checks if there are any GenericPermissions stored for a user
                     .findAny()
-                    //If there are, add them to the list of permissions stored for a user
+                    // If there are, add them to the list of permissions stored for a user
                     .ifPresent(userGenericPermissionList::add);
 
             return userGenericPermissionList;
