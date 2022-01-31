13643ee59a9abda866978148c8ff1ffb36798ae8
Apion <masonbell09@gmail.com>
2022-01-31 16:01:00 -0500

    Got exceptions saving to DB

--- nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/exception/ExceptionRepository.java
+++ nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/exception/ExceptionRepository.java
@@ -1,20 +1,10 @@
 package com.tut.nolebotv2webapi.exception;
 
-import io.micronaut.context.annotation.Executable;
-import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.annotation.RepositoryConfiguration;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
-import io.micronaut.data.jdbc.operations.JdbcRepositoryOperations;
 import io.micronaut.data.model.query.builder.sql.Dialect;
-import io.micronaut.data.operations.RepositoryOperations;
 import io.micronaut.data.repository.CrudRepository;
-import io.micronaut.data.repository.PageableRepository;
-import lombok.NonNull;
-
-import javax.validation.constraints.NotBlank;
-import java.util.UUID;
 
 @JdbcRepository(dialect = Dialect.MYSQL)
-public interface ExceptionRepository extends PageableRepository<TestObj, UUID> {
+public interface ExceptionRepository extends CrudRepository<NoleBotExceptionWrapper, String> {
+
 }
---
+++ shared/src/main/java/com/tut/nolebotshared/exceptions/NoleBotException.java
@@ -0,0 +1,15 @@
+package com.tut.nolebotshared.exceptions;
+
+import java.util.UUID;
+
+public class NoleBotException extends Exception{
+
+    public NoleBotException() {
+    }
+    public NoleBotException(final String errorMsg) {
+        super(errorMsg);
+    }
+    public NoleBotException(final String errorMsg, final Throwable err) {
+        super(errorMsg, err);
+    }
+}
