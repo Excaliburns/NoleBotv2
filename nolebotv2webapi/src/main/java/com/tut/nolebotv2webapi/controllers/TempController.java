4be5432d2b00e46c1ea2e04edddb0c78766f382b
Apion <masonbell09@gmail.com>
2022-01-31 16:01:00 -0500

    Got exceptions saving to DB

--- nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/controllers/TempController.java
+++ nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/controllers/TempController.java
@@ -1,24 +1,16 @@
 package com.tut.nolebotv2webapi.controllers;
 
-import com.tut.nolebotv2webapi.exception.ExceptionRepository;
-import com.tut.nolebotv2webapi.exception.TestObj;
+import com.tut.nolebotshared.exceptions.NoleBotException;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import lombok.NoArgsConstructor;
 
-import javax.inject.Inject;
-
 @Controller
 @NoArgsConstructor
 public class TempController {
-    @Inject
-    private ExceptionRepository repo;
     @Get("/test")
-    public HttpResponse<String> addTestObjToDB() {
-        TestObj obj = new TestObj();
-        obj.setTitle("TestO");
-        repo.save(obj);
-        return HttpResponse.ok("It worked!");
+    public HttpResponse<String> addTestObjToDB() throws NoleBotException {
+        throw new NoleBotException("This is a test error!");
     }
 }
--- shared/src/main/java/com/tut/nolebotshared/exceptions/SnowflakeNotFoundException.java
+++ shared/src/main/java/com/tut/nolebotshared/exceptions/SnowflakeNotFoundException.java
@@ -1,6 +1,6 @@
 package com.tut.nolebotshared.exceptions;
 
-public class SnowflakeNotFoundException extends Exception {
+public class SnowflakeNotFoundException extends NoleBotException {
     public SnowflakeNotFoundException(final String errorMessage) { super (errorMessage); }
     public SnowflakeNotFoundException(final String errorMessage, final Throwable err) { super(errorMessage, err); }
 }
