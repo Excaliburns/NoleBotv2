6f7181663e76d1bb49cd0b0caec09e2be0835be1
Apion <masonbell09@gmail.com>
2022-01-31 16:01:00 -0500

    Got exceptions saving to DB

--- nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/exception/WebExceptionHandler.java
+++ nolebotv2webapi/src/main/java/com/tut/nolebotv2webapi/exception/WebExceptionHandler.java
@@ -1,10 +1,21 @@
 package com.tut.nolebotv2webapi.exception;
 
-import io.micronaut.core.exceptions.ExceptionHandler;
+import com.tut.nolebotshared.exceptions.NoleBotException;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
 
-public class WebExceptionHandler implements ExceptionHandler<Exception> {
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class WebExceptionHandler implements ExceptionHandler<NoleBotException, HttpResponse<String>> {
+    @Inject
+    ExceptionRepository repo;
     @Override
-    public void handle(Exception exception) {
-
+    public HttpResponse<String> handle(HttpRequest request, NoleBotException exception) {
+        NoleBotExceptionWrapper wrapper = NoleBotExceptionWrapper.getWrapperForException(exception);
+        repo.save(wrapper);
+        return HttpResponse.serverError(wrapper.getId());
     }
 }
