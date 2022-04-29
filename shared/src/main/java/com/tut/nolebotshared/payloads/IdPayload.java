8ec21bb63302f139ed3b2148c3b147c172715b61
apion <apion@apionservers.com>
2022-04-29 01:16:07 -0400

    Please for the love of god let this be the last PMD fix

--- shared/src/main/java/com/tut/nolebotshared/payloads/IdPayload.java
+++ shared/src/main/java/com/tut/nolebotshared/payloads/IdPayload.java
@@ -1,7 +1,5 @@
 package com.tut.nolebotshared.payloads;
 
-import com.tut.nolebotshared.entities.GuildUser;
-
 import java.io.Serializable;
 
 public record IdPayload(String userId) implements Serializable {
