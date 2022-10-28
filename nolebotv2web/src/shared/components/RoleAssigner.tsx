9389a44659ca701d977d32681540623f2e52abf3
apion <masonbell09@gmail.com>
2022-10-28 16:55:45 -0400

    Switched back to react-virtuoso

--- nolebotv2web/src/shared/components/RoleAssigner.tsx
+++ nolebotv2web/src/shared/components/RoleAssigner.tsx
@@ -1,148 +1,42 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete, { autocompleteClasses } from '@mui/material/Autocomplete';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import ListSubheader from '@mui/material/ListSubheader';
-import Popper from '@mui/material/Popper';
-import { useTheme, styled } from '@mui/material/styles';
-import { VariableSizeList, ListChildComponentProps } from 'react-window';
-import Typography from '@mui/material/Typography';
-import {useState} from "react";
+
+import {useTheme, Theme} from '@mui/material/styles';
+import {Virtuoso} from 'react-virtuoso'
 import {GuildRole, GuildUser} from "../../entities/JavaGenerated";
 import {useAxios} from "../../util/AxiosProvider";
-import {Box, Button, SvgIcon} from "@mui/material";
+import {Autocomplete, Box, Button, List, ListItemText, SvgIcon, TextField} from "@mui/material";
 import {useStateMachine} from "little-state-machine";
+import React, {useState} from "react";
 
-const LISTBOX_PADDING = 8; // px
-
-function renderRow(props: ListChildComponentProps) {
-    const { data, index, style } = props;
-    const dataSet = data[index];
-    const inlineStyle = {
-        top: (style.top as number) + LISTBOX_PADDING,
-        flexGrow: 1
-    };
-
-    if (dataSet.hasOwnProperty('group')) {
-        return (
-            <ListSubheader key={dataSet.key} component="div" style={inlineStyle}>
-                {dataSet.group}
-            </ListSubheader>
-        );
-    }
-
-    return (
-        <div style={{
-            display: "flex",
-            alignItems: "center"
-        }}>
-            <SvgIcon>
-                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-            </SvgIcon>
-            <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
-                {dataSet[1]}
-            </Typography>
-        </div>
-    );
-}
-
-const OuterElementContext = React.createContext({});
-
-const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
-    const outerProps = React.useContext(OuterElementContext);
-    return <div ref={ref} {...props} {...outerProps} />;
-});
-
-function useResetCache(data: any) {
-    const ref = React.useRef<VariableSizeList>(null);
-    React.useEffect(() => {
-        if (ref.current != null) {
-            ref.current.resetAfterIndex(0, true);
-        }
-    }, [data]);
-    return ref;
-}
-
-// Adapter for react-window
-const ListboxComponent = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLElement>
-    >(function ListboxComponent(props, ref) {
-    const { children, ...other } = props;
+const Listbox = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLElement>>((props, ref) => {
+    const {children, ...other} = props;
     const itemData: React.ReactChild[] = [];
     (children as React.ReactChild[]).forEach(
-        (item: React.ReactChild & { children?: React.ReactChild[] }) => {
+        (item: React.ReactChild ) => {
             itemData.push(item);
-            itemData.push(...(item.children || []));
         },
     );
-
-    const theme = useTheme();
-    const smUp = useMediaQuery(theme.breakpoints.up('sm'), {
-        noSsr: true,
-    });
-    const itemCount = itemData.length;
-    const itemSize = smUp ? 36 : 48;
-
-    const getChildSize = (child: React.ReactChild) => {
-        if (child.hasOwnProperty('group')) {
-            return 48;
+    return <div {...other} ref={ref}>
+        <Virtuoso
+            style={{ height: '400px' }}
+            totalCount={itemData.length}
+            itemContent={(index) => {
+                return itemData[index]
+            }
         }
-
-        return itemSize;
-    };
-
-    const getHeight = () => {
-        if (itemCount > 8) {
-            return 8 * itemSize;
-        }
-        return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
-    };
-
-    const gridRef = useResetCache(itemCount);
-
-    return (
-        <div ref={ref}>
-            <OuterElementContext.Provider value={other}>
-                <VariableSizeList
-                    itemData={itemData}
-                    height={getHeight() + 2 * LISTBOX_PADDING}
-                    width="100%"
-                    ref={gridRef}
-                    outerElementType={OuterElementType}
-                    innerElementType="ul"
-                    itemSize={(index) => getChildSize(itemData[index])}
-                    overscanCount={5}
-                    itemCount={itemCount}
-                >
-                    {renderRow}
-                </VariableSizeList>
-            </OuterElementContext.Provider>
-        </div>
-    );
-});
-
-const StyledPopper = styled(Popper)({
-    [`& .${autocompleteClasses.listbox}`]: {
-        boxSizing: 'border-box',
-        '& ul': {
-            padding: 0,
-            margin: 0,
-        },
-    },
-});
-
+        />
+    </div>
+})
 export default function RoleAssigner() {
-    const selectStyle = {
-        inputProps: {
-            width: "100vw",
-            marginY: "1vh",
-            backgroundColor: "white",
-        },
+    const mainBoxStyle = {
+        margin: "1vh",
+        "& .MuiFormControl-root": {
+            margin: "1vh"
+        }
     }
     const [memberOptions, setMemberOptions] = useState<{value: string, id: string}[]>([])
     const [roleOptions, setRoleOptions] = useState<{value: string, id: string}[]>([])
     const [search, setSearch] = useState<string>("")
+    const theme: Theme = useTheme()
     let [selectedMembers, setSelectedMembers] = React.useState<{value: string, id: string}[]>([])
     let [selectedRoles, setSelectedRoles] = React.useState<{value: string, id: string}[]>([])
     const {state} = useStateMachine()
@@ -192,58 +86,60 @@ export default function RoleAssigner() {
             })
     }, [state.jwt])
     return (
-        <Box sx={{marginY: "1vh"}}>
+        <Box sx={mainBoxStyle}>
             <Autocomplete
-                sx={{marginY: "1vh"}}
-                multiple
-                id="virtualize-demo"
-                noOptionsText={"Start typing..."}
-                disableListWrap
-                PopperComponent={StyledPopper}
-                ListboxComponent={ListboxComponent}
-                options={roleOptions}
-                getOptionLabel={(option) =>
-                    typeof option === 'string' ? option : option.value
+                multiple={true}
+                renderInput={
+                    (params) => {
+                        return <TextField {...params} label={"Roles"}/>
+                    }
                 }
-                isOptionEqualToValue={(option, value) => {
-                    return option.value === value.value
+                options={roleOptions}
+                getOptionLabel={(option) => {
+                    return option.value
                 }}
-                renderInput={(params) => {
-                    return <TextField {...params} label="Roles" />}}
-                renderOption={(props, option) => [props, option.value] as React.ReactNode}
-                // TODO: Post React 18 update - validate this conversion, look like a hidden bug
-                renderGroup={(params) => params as unknown as React.ReactNode}
+                ListboxComponent={Listbox}
+                renderOption={(props:object, option:{value: string, id:string}) => {
+                    return <Box {...props}>
+                        <SvgIcon>
+                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                        </SvgIcon>
+                        {option.value}
+                    </Box>
+                }}
+                onInputChange={(event, newVal, reason) => {
+                    setSearch(newVal)
+                }}
                 onChange={(event, value, reason) => {
                     setSelectedRoles(value)
-                }}
-            />
+                }}/>
+
             <Autocomplete
-                sx={{marginY: "1vh"}}
-                multiple
-                id="virtualize-demo"
-                noOptionsText={"Start typing..."}
-                disableListWrap
-                PopperComponent={StyledPopper}
-                ListboxComponent={ListboxComponent}
-                filterOptions={(x => x)}
-                options={memberOptions}
-                getOptionLabel={(option) =>
-                    typeof option === 'string' ? option : option.value
+                multiple={true}
+                renderInput={
+                    (params) => {
+                        return <TextField {...params} label={"Members"}/>
+                    }
                 }
-                isOptionEqualToValue={(option, value) => {
-                    return option.value === value.value
+                options={memberOptions}
+                getOptionLabel={(option) => {
+                    return option.value
                 }}
-                renderInput={(params) => <TextField {...params} label="Members" />}
-                renderOption={(props, option) => [props, option.value] as React.ReactNode}
-                // TODO: Post React 18 update - validate this conversion, look like a hidden bug
-                renderGroup={(params) => params as unknown as React.ReactNode}
-                onInputChange={(event, newInputValue) => {
-                    setSearch(newInputValue);
+                ListboxComponent={Listbox}
+                renderOption={(props:object, option:{value: string, id:string}) => {
+                    return <Box {...props}>
+                        <SvgIcon>
+                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                        </SvgIcon>
+                        {option.value}
+                    </Box>
+                }}
+                onInputChange={(event, newVal, reason) => {
+                    setSearch(newVal)
                 }}
                 onChange={(event, value, reason) => {
                     setSelectedMembers(value)
-                }}
-            />
+                }}/>
             <Box sx={{display: "flex", justifyContent: "right"}}>
                 <Button variant={"contained"} onClick={clickButton}>Submit</Button>
             </Box>
